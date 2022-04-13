6519f746b044cc3b809bb2a85afb599c4473e27c
dhruv50ae <linkdp2012@gmail.com>
2022-04-13 15:23:37 +0100

    Used returns on leap year calculator

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-def formatName(fName, lName):
-    alpha = fName.title()
-    beta = lName.title()
-    return f"{alpha} {beta}"
+def isLeap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
 
 
-print(formatName("jEfF", "BezOs"))
+def daysInMonth(year, month):
+    if month > 12 and month == 2:
+        return "Invalid month"
+    monthDays = [31, 28, 31, 30, 31, 30, 31, 31, 30]
+    if isLeap(year) and month == 2:
+        return 29
+    return monthDays[month-1]
 
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = daysInMonth(year, month)
+
+print(days)
