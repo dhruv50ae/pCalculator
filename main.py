9241eee5ace2c346555d723f65c7f4e923354123
dhruv50ae <linkdp2012@gmail.com>
2022-04-13 16:38:12 +0100

    Created the foundations of the calc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Calculator
+
 def add(n1, n2):
     return n1 + n2
 
@@ -15,20 +17,18 @@ def divide(n1, n2):
 
 
 operations = {
-    "+", add,
-    "-", subtract,
-    "*", multiply,
-    "/", divide,
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
 }
 
 num1 = int(input("What's the first number?: "))
-
 for symbol in operations:
     print(symbol)
-
 operationSymbol = input("Pick an operation from the line above: ")
-
 num2 = int(input("What's the second number?: "))
-
 calculationFunction = operations[operationSymbol]
 answer = calculationFunction(num1, num2)
+
+print(f"{num1} {operationSymbol} {num2} = {answer}")
